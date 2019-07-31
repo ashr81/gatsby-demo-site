4581d5e376ea6b580273dd6dc9cf3fab3f426525
ashr81 <ashrithreddi81@gmail.com>
2019-08-01 01:15:59 +0530

    Adds in description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Code JAM",
+    title: "Gatsby Theme Starter",
     author: {
       name: 'Ashrith',
       github: 'https://www.github.com/ashr81',
       phone: '+91-43243545345',
       twitter: 'https://twitter.com/ashrith381',
       image_url: 'https://pbs.twimg.com/profile_images/427296555805048832/mSI_6vM7_400x400.jpeg'    
-    }
+    },
+    description: 'Gatsby theme starter using contentful CMS'
   },
   plugins: [{
     resolve: "@ashr81/gatsby-theme-profile-builder",
